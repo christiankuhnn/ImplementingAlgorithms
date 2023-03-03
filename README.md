db34ce384af6a9e41f1a97d4418a123a1685fffb
christiankuhnn <kuhnc@udel.edu>
2023-03-03 10:39:48 -0500

    final sub,ission

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ImplementingAlgorithms
 assignment #7 for algorithms
+The program reads a sequence of student logs from a file and calculates the maximum, most recent, and average student status based on their page IDs and submission scores. The output is a series of sorted student status reports, with each report containing the student's ID, lowest page ID, latest page ID, and average submission score.
+
+The program consists of three main functions:
+
+read_logs: Parses the input file and stores the logs for each student in a dictionary.
+
+Algorithmic runtime: O(n), where n is the number of logs.
+calculate_student_statuses: Calculates the student statuses based on their logs and returns a list of tuples containing the student ID, lowest page ID, latest page ID, and average submission score for each student.
+
+Algorithmic runtime: O(n), where n is the number of students.
+print_student_statuses: Prints the student statuses in the desired format.
+
+Algorithmic runtime: O(n log n), where n is the number of students (due to sorting).
--- tests.py
+++ tests.py
@@ -1,13 +1,42 @@
 import unittest
 import answer
-from answer import print_results
 
-class TestLast(unittest.TestCase):
-    def tests_assign_function(self):
-        actual = answer.print_results(["2", "507 P 1000 1", "1 S 6 2", "1 T 98 3"])
-        self.assertEqual(actual, ('507 1000 1000 6'))
+class TestReadLogs(unittest.TestCase):
+    def test_read_logs_valid_input(self):
+        input_file = "valid_input.txt"
+        expected_output = [
+            (1, 'P', 1, 1),
+            (2, 'S', 100, 2),
+            (3, 'T', 98, 3)
+        ]
+        self.assertEqual(answer.read_logs(input_file), expected_output)
 
+    def test_read_logs_empty_input(self):
+        input_file = "empty_input.txt"
+        expected_output = []
+        self.assertEqual(answer.read_logs(input_file), expected_output)
     
+class TestCalculateResults(unittest.TestCase):
+    def test_calculate_results_valid_input(self):
+        logs = [(1, 'P', 100, 1), (1, 'S', 6, 2), (1, 'T', 98, 3)]
+        expected_output = {'507': (1000, 1000, 6)}
+        self.assertEqual(answer.calculate_results(logs), expected_output)
+
+    def test_calculate_results_empty_input(self):
+        logs = []
+        expected_output = {}
+        self.assertEqual(answer.calculate_results(logs), expected_output)
+
+class TestPrintResults(unittest.TestCase):
+    def test_print_results_valid_input(self):
+        results = {'507': (1000, 1000, 6)}
+        expected_output = '507 1000 1000 6\n'
+        self.assertEqual(answer.print_results(results), expected_output)
+
+    def test_print_results_empty_input(self):
+        results = {}
+        expected_output = ''
+        self.assertEqual(answer.print_results(results), expected_output)
 
 if __name__ == '__main__':
     unittest.main()
